d046ec3e0b1f5909c23fd8373fc6b1e97c57024f
Onur Elhakan <72891423+oelhakan@users.noreply.github.com>
2021-01-03 16:31:04 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal" , 1,1,5,5);
+
+        Dog dog = new Dog("Yorke", 8,20,4,2,1,20,"long silky");
+        dog.walk();
+        dog.run();
+
+    }
+}
